dbcbcd4767dec99d0c63c924964348e59835c1c2
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-05 03:53:34 +0600

    Design Feed Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
+import Feed from "@/components/Feed";
+import Share from "@/components/Share";
+
 const Homepage = () => {
-  return <div className="">homepage</div>;
+  return (
+    <div className="">
+      <div className="flex justify-evenly border-b border-borderGray px-4 pt-4 font-bold text-textGray">
+        <Link
+          href="/"
+          className="flex items-center border-b-4 border-iconBlue pb-3"
+        >
+          For you
+        </Link>
+        <Link href="/" className="flex items-center pb-3">
+          Following
+        </Link>
+      </div>
+      <Share />
+      <Feed />
+    </div>
+  );
 };
 
 export default Homepage;
