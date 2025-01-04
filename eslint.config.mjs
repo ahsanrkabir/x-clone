552a05d3b05dc6c603b8192c26ff6676804a42ab
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-05 00:16:54 +0600

    Setup ESLint & Prettier, Implement Layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,12 +11,39 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended", "prettier"),
   {
     rules: {
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin", // Built-in types are first
+            "external", // External libraries
+            "internal", // Internal modules
+            ["parent", "sibling"], // Parent and sibling types can be mingled together
+            "index", // Then the index file
+            "object" // Object imports
+          ],
+          "newlines-between": "always",
+          "pathGroups": [
+            {
+              "pattern": "@app/**",
+              "group": "external",
+              "position": "after"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          }
+        }
+      ],
       "no-unused-vars": "warn",
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
     },
+    "ignores": ["components/ui/**"],
   },
 ];
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+
+import LeftBar from "@/components/LeftBar";
+import RightBar from "@/components/RightBar";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +11,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="mx-auto flex max-w-screen-md justify-between lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl">
+          <div className="px-2 xsm:px-4 xxl:px-8">
+            <LeftBar />
+          </div>
+          <div className="flex-1 border-x border-gray-100 lg:min-w-[600px]">
+            {children}
+          </div>
+          <div className="ml-4 hidden flex-1 md:ml-8 lg:flex">
+            <RightBar />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
