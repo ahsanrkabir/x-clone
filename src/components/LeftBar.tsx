9643ccf54db24ab3087e7fa196c36c16f1ebacc2
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-05 01:28:27 +0600

    Design Left Bar

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,5 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { menuList } from "@/constants";
+
 const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <div className="sticky top-0 flex h-screen flex-col justify-between pb-8 pt-2">
+      {/* LOGO MENU BUTTON */}
+      <div className="flex flex-col items-center gap-2 text-base lg:gap-3 xxl:items-start xxl:gap-4">
+        {/* LOGO */}
+        <Link href="/" className="rounded-full p-2 hover:bg-[#181818]">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
+        </Link>
+
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-3">
+          {menuList.map((item) => (
+            <Link
+              key={item.id}
+              href={item.link}
+              className="flex items-center gap-4 rounded-full p-2 hover:bg-[#181818]"
+            >
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                width={24}
+                height={24}
+              />
+              <span className="hidden xxl:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+
+        {/* BUTTON */}
+        <Link
+          href="/"
+          className="flex size-12 items-center justify-center rounded-full bg-white text-black xxl:hidden"
+        >
+          <Image src="icons/post.svg" alt="new post" width={24} height={24} />
+        </Link>
+        <Link
+          href="/"
+          className="hidden rounded-full bg-white px-20 py-2 font-bold text-black xxl:block"
+        >
+          Post
+        </Link>
+      </div>
+
+      {/* USER */}
+      <div className="flex items-center justify-between rounded-full p-2 hover:bg-[#181818]">
+        <div className="flex items-center gap-2">
+          <div className="relative size-10 overflow-hidden rounded-full">
+            <Image src="/general/avatar.png" alt="avatar" fill />
+          </div>
+          <div className="hidden flex-col xxl:flex">
+            <span className="font-bold">Ahsan Kabir</span>
+            <span className="text-sm text-textGray">@ahsanrkabir</span>
+          </div>
+        </div>
+        <div className="hidden cursor-pointer font-bold xxl:block">...</div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftBar;
